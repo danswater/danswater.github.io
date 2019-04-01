ec73e1e5e81aeb0637ce565be0e3271f520aa38a
Francis Kennet V. Lamayo <dansisabot@gmail.com>
2019-04-01 15:13:25 +0800

    initial commit

---
+++ precache-manifest.9bfe1b587352f5f3be10e1b527a4c76b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b7b230b0bdb4cc57608",
+    "url": "/My-Landing-Page/static/js/runtime~main.4f5ad23c.js"
+  },
+  {
+    "revision": "5bee143864ebe3b76304",
+    "url": "/My-Landing-Page/static/js/main.3d639f84.chunk.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/My-Landing-Page/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "5bee143864ebe3b76304",
+    "url": "/My-Landing-Page/static/css/main.13b9140f.chunk.css"
+  },
+  {
+    "revision": "61e68709921040be525236d133436b5a",
+    "url": "/My-Landing-Page/index.html"
+  }
+];
